820bbdf4263f2cd991278485c4b21799c1b851b5
Talya <talya.stern@gmail.com>
2023-01-15 16:43:06 +0800

    basic bingo board

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,11 @@
 import React from 'react';
 import './App.css';
-
-function requestOptions(method: string, body?: any) {
-  const headers = {
-    method: method,
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(body)
-  };
-
-  return headers
-}
-
-function call(url: string, requestOptions: any) {
-  return fetch(url, requestOptions)
-    .catch((err) => {
-      console.log(err.message);
-    });
-}
+import Board from './components/Board'
 
 function App() {
-  const url = "https://b39b828gcb.execute-api.ap-northeast-1.amazonaws.com";
-
-  const handleClick = (e: any) => {
-    call(`${url}/toggleCheckItem`, requestOptions('POST', { id: "Adela", name: "hello"}));
-  }
-
   return (
     <div className="App">
-      <header className="App-header">
-        <button onClick={handleClick}>Click me!</button>
-      </header>
+      <Board />
     </div>
   );
 }

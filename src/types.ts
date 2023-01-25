6fe0f0da4bc4ad84c84bc84fecc4094ce20b46d9
Talya <talya.stern@gmail.com>
2023-01-25 16:08:37 +0800

    fix lint

--- src/types.ts
+++ src/types.ts
@@ -1,21 +1,20 @@
 export type UserBoardType = {
-    [key: string]: BoardItemType
+  [key: string]: BoardItemType
 }
 export type BoardItemType = {
-    id: string,
-    text: string,
-    checked: boolean
+  id: string,
+  text: string,
+  checked: boolean
 }
 
 export const BANSKO_BOARD_ITEMS = {
-    bansko_1: 'Hot Springs',
-    bansko_2: 'Hike Pirin mountains',
-    bansko_3: 'Watch a sunset',
-    bansko_4: 'Picnic in Pirin forest',
-    bansko_5: 'Have a Banitza',
-    bansko_6: 'Visit a co-work',
-    bansko_7: 'Hike to Bezbog hut',
-    bansko_8: 'Smoothie at Coconut',
-    bansko_9:'Belizmata lake'
+  bansko_1: 'Hot Springs',
+  bansko_2: 'Hike Pirin mountains',
+  bansko_3: 'Watch a sunset',
+  bansko_4: 'Picnic in Pirin forest',
+  bansko_5: 'Have a Banitza',
+  bansko_6: 'Visit a co-work',
+  bansko_7: 'Hike to Bezbog hut',
+  bansko_8: 'Smoothie at Coconut',
+  bansko_9: 'Belizmata lake'
 }
-  

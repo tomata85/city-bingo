1a05be2f81f1eaf6fcb8c913e253e26983b2c1a0
Talya <talya.stern@gmail.com>
2023-01-27 13:22:09 +0800

    fix no board in local storage endge case

--- src/utils.ts
+++ src/utils.ts
@@ -14,7 +14,11 @@ export function getInitialUserBoard (_: { userId: string, destinationId: string 
 }
 
 export function getBoardFromStorage (): UserBoardType | undefined {
-  return JSON.parse(localStorage.getItem('board') ?? '')
+  const board = localStorage.getItem('board')
+
+  if (board === '{}' || board === null) {
+    return undefined
+  } return JSON.parse(board)
 }
 
 export function storeBoard (board: UserBoardType): void {

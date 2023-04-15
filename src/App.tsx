10a0bbe3c16689b19fe22d106afdd3b0ad670a53
Talya <talya.stern@gmail.com>
2023-04-15 17:33:25 +0700

    showing did it page on click

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import React, { type ReactElement } from 'react'
 import './App.css'
 import Board from './components/Board'
-import DidItPage from './components/DidItPage'
 
 function App (): ReactElement {
   return (
     <div className="App">
       <header>Welcome back Yulia</header>
       <h1 className="title">Bansko Bingo</h1>
-      {/* <Board userId="Oren Chazan" destinationId="Bansko" /> */}
-      <DidItPage/>
+      <Board userId="Oren Chazan" destinationId="Bansko" />
     </div>
   )
 }
--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -3,7 +3,8 @@ import BoardItem from '../components/BoardItem'
 import './styles.css'
 import { getBoardFromStorage, storeBoard } from '../logic/local-storage'
 import { getInitialUserBoard, isBoardWin } from '../logic/board'
-import { UserBoardType } from '../types'
+import { BoardItemType, UserBoardType } from '../types'
+import DidItPage from './DidItPage'
 
 export default function Board (props: {
   userId: string
@@ -13,6 +14,7 @@ export default function Board (props: {
     getBoardFromStorage() ?? getInitialUserBoard(props)
   )
 
+  const [selectedItem, setSelectedItem] = useState<BoardItemType | null>(null)
   const [board, setBoard] = useState<UserBoardType>(getUserBoard())
   const [isWin, setIsWin] = useState<boolean>(false)
 
@@ -22,18 +24,23 @@ export default function Board (props: {
   }, [board])
 
   const onClickItem = (itemId: string): void => {
-    setBoard({
-      ...board,
-      [itemId]: { ...board[itemId], checked: !board[itemId].checked }
-    })
+    // setBoard({
+    //   ...board,
+    //   [itemId]: { ...board[itemId], checked: !board[itemId].checked }
+    // })
+    setSelectedItem(board[itemId])
   }
 
   return (
-    <div className="board-container">
-      {Object.values(board).map((item) => (
-        <BoardItem key={item.id} item={item} onClick={onClickItem} />
-      ))}
-      <div>{ isWin ? 'Yay you win!' : ''}</div>
-    </div>
+    <>
+      { (selectedItem != null)
+        ? <DidItPage/>
+        : <div className="board-container">
+          {Object.values(board).map((item) => (
+            <BoardItem key={item.id} item={item} onClick={onClickItem} />
+          ))}
+          <div>{ isWin ? 'Yay you win!' : ''}</div>
+        </div> }
+    </>
   )
 }

a1f050174e084b87417b3855637ab15909e80e92
Talya <talya.stern@gmail.com>
2023-08-06 12:43:10 +0300

    add typogrpahy

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React, { type ReactElement } from 'react'
 import './App.css'
 import Main from './components/Main'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+
+const theme = createTheme({
+  typography: {
+    fontFamily: ['Inter', 'Roboto', 'sans-serif'].join(',')
+  }
+})
 
 function App (): ReactElement {
   return (
-    <div className="App">
-      <Main />
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Main />
+      </div>
+    </ThemeProvider>
   )
 }
 
--- src/components/ItemPages/ItemDialog.tsx
+++ src/components/ItemPages/ItemDialog.tsx
@@ -3,6 +3,7 @@ import { BoardInstanceItemType } from '../../types'
 import DidItPage from './DidItPage'
 import { Dialog, DialogTitle } from '@mui/material'
 import { useTranslation } from 'react-i18next'
+import './styles.css'
 
 export interface ItemDialogProps {
   item: BoardInstanceItemType
@@ -14,9 +15,9 @@ export default function ItemDialog (props: ItemDialogProps): ReactElement {
   const { t } = useTranslation()
 
   return (
-    <Dialog onClose={onClose} open={true}>
-      <DialogTitle>{t(item.id)}</DialogTitle>
+    <Dialog className='item_pages' onClose={onClose} open={true}>
+      <DialogTitle><h1>{t(item.id)}</h1></DialogTitle>
+      <DidItPage {...props}/>
     </Dialog>
-  // <DidItPage {...props}/>
   )
 }

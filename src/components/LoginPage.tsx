17f924e9bbaa6863945573509e1a214be91228d0
Talya <talya.stern@gmail.com>
2023-09-11 09:25:02 +0300

    cosmetics

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -1,10 +1,13 @@
-import { TextField } from '@mui/material'
+import { Box, TextField } from '@mui/material'
 import React, { useState, type ReactElement, useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
 import FooterPane from './Infrastructure/FooterPane'
 import FooterPaneButton from './Infrastructure/FooterPaneButton'
 import { User } from '../types'
-import { getLoggedInUserFromStorage, storeLoggedInUser } from '../logic/local-storage'
+import {
+  getLoggedInUserFromStorage,
+  storeLoggedInUser
+} from '../logic/local-storage'
 
 export interface LoginPageProps {
   onLogin: (user: User) => void
@@ -30,30 +33,32 @@ export default function LoginPage (props: LoginPageProps): ReactElement {
 
   return (
     <>
-      <h3>{t('login_name')}</h3>
-      <TextField
-        sx={{
-          width: '100%'
-        }}
-        id="outlined-basic"
-        label={t('login_name')}
-        variant="outlined"
-        onChange={(event: any) => {
-          setName(event.target.value)
-        }}
-      />
-      <h3>{t('login_email')}</h3>
-      <TextField
-        sx={{
-          width: '100%'
-        }}
-        id="outlined-basic"
-        label={t('login_email')}
-        variant="outlined"
-        onChange={(event: any) => {
-          setEmail(event.target.value)
-        }}
-      />
+      <Box sx={{ margin: '30px', textAlign: 'left' }}>
+        <h3>{t('login_name')}</h3>
+        <TextField
+          sx={{
+            width: '100%'
+          }}
+          id="outlined-basic"
+          label={t('login_name')}
+          variant="outlined"
+          onChange={(event: any) => {
+            setName(event.target.value)
+          }}
+        />
+        <h3>{t('login_email')}</h3>
+        <TextField
+          sx={{
+            width: '100%'
+          }}
+          id="outlined-basic"
+          label={t('login_email')}
+          variant="outlined"
+          onChange={(event: any) => {
+            setEmail(event.target.value)
+          }}
+        />
+      </Box>
       <FooterPane>
         <FooterPaneButton text={t('login_save')} onClick={onLoginClick} />
       </FooterPane>

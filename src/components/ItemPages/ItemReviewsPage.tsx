a325257d0fcecf959a241abbcd7a0ba9cbc981ae
Talya <talya.stern@gmail.com>
2023-09-14 12:07:25 +0300

    intial reviews page

--- src/components/ItemPages/ItemReviewsPage.tsx
+++ src/components/ItemPages/ItemReviewsPage.tsx
@@ -1,6 +1,13 @@
 import React, { ReactElement } from 'react'
 import { ItemPagesProps } from './ItemPagesContainer'
+import { Box } from '@mui/system'
+import { useTranslation } from 'react-i18next'
 
 export default function ItemReviewsPage (props: ItemPagesProps): ReactElement {
-  return (<></>)
+  const { t } = useTranslation()
+
+  return (
+  <Box>
+    {t('reviews_soon')}
+  </Box>)
 }

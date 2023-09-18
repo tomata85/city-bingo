c7a69fcc78a995a15f2e85671adc2d1322db64a9
Talya <talya.stern@gmail.com>
2023-09-18 15:36:43 +0300

    about ut

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,53 +1,52 @@
-import React, { type ReactElement, useState, useEffect } from 'react'
+import React, { type ReactElement, useState } from 'react'
 import BoardPage from './board/BoardPage'
 import Button from '@mui/material/Button'
-// TODO: bring back translation
-import TranslateIcon from '@mui/icons-material/Translate'
-import { useTranslation } from 'react-i18next'
 import AppBar from '@mui/material/AppBar'
 import {
   BottomNavigation,
   BottomNavigationAction,
   Box,
-  Drawer,
   Link,
   Paper,
-  Stack,
   Toolbar,
   Typography
 } from '@mui/material'
 import { BINGO_SIZE, User } from '../types'
 import LoginPage from './LoginPage'
 import FaceIcon from '@mui/icons-material/Face'
-import HelpOutlineIcon from '@mui/icons-material/HelpOutline'
 import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline'
 import LocalCafeOutlinedIcon from '@mui/icons-material/LocalCafeOutlined'
 import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined'
 import './styles.css'
-import { t } from 'i18next'
+import { AboutUsPage } from './bottom-drawer/AboutUsPage'
+
+const enum DisplayedPages {
+  Game,
+  AboutUs,
+  Feedback,
+}
 
 export default function Main (): ReactElement {
   const APP_MARGIN = BINGO_SIZE < 5 ? 'auto 30px' : 'auto 15px'
-  const { i18n } = useTranslation()
-  const [lang, setLang] = useState<string>(i18n.language)
   const [user, setUser] = useState<User>()
-  const [showHelp, setShowHelp] = useState<boolean>(false)
-
-  useEffect(() => {
-    void i18n.changeLanguage(lang)
-  }, [lang])
-
-  const onChangeLanguage = (): void => {
-    const updatedLang = lang === 'en' ? 'he' : 'en'
-    setLang(updatedLang)
-  }
+  const [currentPageIndex, setCurrentPageIndex] = useState<number>(0)
 
   const onLogin = (user: User) => {
     setUser(user)
+    setCurrentPageIndex(DisplayedPages.Game)
   }
 
-  const onShowHelp = () => {
-    setShowHelp(true)
+  const selectedPage = () => {
+    switch (currentPageIndex) {
+      case DisplayedPages.Game:
+        if (user != null) {
+          return <BoardPage user={user} destinationId="Bansko" />
+        }
+        return <LoginPage onLogin={onLogin} />
+      case DisplayedPages.AboutUs:
+        return <AboutUsPage />
+    }
+    return <></>
   }
 
   return (
@@ -66,13 +65,6 @@ export default function Main (): ReactElement {
             >
               City Bingo
             </Typography>
-            {/* <Button
-              color="inherit"
-              onClick={onChangeLanguage}
-              startIcon={<TranslateIcon />}
-            >
-              {lang}
-            </Button> */}
             {user != null && (
               <Button color="inherit" startIcon={<FaceIcon />} size="large">
                 {user.name}
@@ -81,24 +73,25 @@ export default function Main (): ReactElement {
           </Toolbar>
         </AppBar>
       </Box>
-      <Box sx={{ margin: APP_MARGIN }}>
-        {user === undefined
-          ? (
-          <LoginPage onLogin={onLogin} />
-            )
-          : (
-          <BoardPage user={user} destinationId="Bansko" />
-            )}
-      </Box>
+      <Box sx={{ margin: APP_MARGIN }}>{selectedPage()}</Box>
       <Paper
         sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }}
         elevation={3}
       >
-          <BottomNavigation>
-            <BottomNavigationAction icon={<HomeOutlinedIcon />} />
-            <BottomNavigationAction icon={<LocalCafeOutlinedIcon />} />
-            <BottomNavigationAction icon={<ChatBubbleOutlineIcon />} />
-          </BottomNavigation>
+        <BottomNavigation value={currentPageIndex}>
+          <BottomNavigationAction
+          onClick={() => {
+            setCurrentPageIndex(DisplayedPages.Game)
+          }}
+           icon={<HomeOutlinedIcon />} />
+          <BottomNavigationAction
+            onClick={() => {
+              setCurrentPageIndex(DisplayedPages.AboutUs)
+            }}
+            icon={<LocalCafeOutlinedIcon />}
+          />
+          <BottomNavigationAction icon={<ChatBubbleOutlineIcon />} />
+        </BottomNavigation>
       </Paper>
     </>
   )
